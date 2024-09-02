20d885a16d777b7b584adc431280a84847d1218c
noothangimounika <noothangimounika@gmail.com>
2024-09-02 17:25:45 +0530

    Harmony Contact webpage

---
+++ javascript.js
@@ -0,0 +1,49 @@
+// // alert("Hello Everyone");
+
+// var name="Mounika";
+// var name="Noothangi"
+// console.log(name);
+// console.log(name);
+// // console.log("js is working");
+// let color="black";
+// // let color="blue";
+// console.log(color);
+
+// const address="my adress is perminate";
+// // const address="my adress is perminate";
+// console.log(address);
+
+// var name="Mounika";
+// var id=123;
+// var salary=15000.04;
+// Boolean=true;
+// console.log(name);
+// console.log(id);
+// console.log(salary);
+
+// text=10<5;
+// console.log(text);
+
+// text=5<10;
+// console.log(text);
+
+// document.write("salry is" +salary)
+
+
+document.write("This is js messages <br>  ");
+document.write(12345.0);
+
+document.write("<h1> This is h1 heading");
+document.write("<h2> This is h2 heading");
+document.write("<h3> This is h3 heading");
+document.write("<h4> This is h4 heading");
+document.write("<h5> This is h5 heading");
+document.write("<h6> This is h6 heading<br>");
+document.write("<button>hello   ")
+
+
+
+
+
+
+
